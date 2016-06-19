6029786b38ce09df0aebc883b6cac33fb788380f
Ugo Arzur <ugo@payname.fr>
2016-06-19 09:00:21 +0200

    add twitter / facebook / devianart

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -4,6 +4,10 @@
   ==================
       ADMIN PAGE
   ==================
+  add_action( $tag, $function_to_add, $priority, $accepted_args );
+  add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+  add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
+  register_setting( $option_group, $option_name, $sanitize_callback );
  */
 
 function crevette_add_admin_page(){
@@ -14,26 +18,62 @@ function crevette_add_admin_page(){
   add_submenu_page( 'crevette_theme_settings', 'Crevette Theme Options', 'General', 'manage_options', 'crevette_theme_settings', 'crevette_theme_create_page' );
   add_submenu_page( 'crevette_theme_settings', 'Crevette Options CSS', 'Custom CSS', 'manage_options', 'crevette_theme_css', 'crevette_theme_css_settings' );
 
-  // Activate custom settings
+  // Activate custom crevette
   add_action( 'admin_init', 'crevette_custom_settings', "manage_options", 'crevette_custom_settings' );
 }
 add_action('admin_menu', 'crevette_add_admin_page');
+
 function crevette_theme_create_page(){
   require_once(get_template_directory().'/inc/templates/crevette-admin.php');
 }
 function crevette_theme_css_settings(){
   echo '<h1>Hey c\'est la page du css</h1>';
 }
+// Bundle functions of custom admin page
 function crevette_custom_settings(){
   register_setting( 'crevette-settings-group', 'first_name');
+  register_setting( 'crevette-settings-group', 'last_name');
+  register_setting( 'crevette-settings-group', 'twitter_account', 'crevette_sanitize_twitter_account');
+  register_setting( 'crevette-settings-group', 'devianart_account');
+  register_setting( 'crevette-settings-group', 'facebook_account');
+
   add_settings_section( 'crevette_sidebar-options', 'Sidebar Options', 'crevette_sidebar_options', 'crevette_theme_settings' );
-  add_settings_field( 'sidebar-name', 'Prénom', 'crevette_sidebar_name', 'crevette_theme_settings', 'crevette_sidebar-options');
+
+  add_settings_field( 'sidebar-name', 'Nom complet', 'crevette_sidebar_name', 'crevette_theme_settings', 'crevette_sidebar-options');
+  add_settings_field( 'sidebar-twitter', 'Compte Twitter', 'crevette_sidebar_twitter', 'crevette_theme_settings', 'crevette_sidebar-options');
+  add_settings_field( 'sidebar-facebook', 'Compte Facebook', 'crevette_sidebar_facebook', 'crevette_theme_settings', 'crevette_sidebar-options');
+  add_settings_field( 'sidebar-devianart', 'Compte Deviant Art', 'crevette_sidebar_devianart', 'crevette_theme_settings', 'crevette_sidebar-options');
+}
+
+// Sanitization settings
+function crevette_sanitize_twitter_account( $input ){
+  $output = sanitize_text_field( $input );
+  return $output;
+}
+
+// Bunch of functions to display html
+function crevette_sidebar_devianart(){
+  $devianartAccount = esc_attr(get_option('devianart_account'));
+  echo '<input type="text" name"twitter_account" placeholder="Adresse Deviant Art" value="'.$devianartAccount.'">';
+  var_dump($devianartAccount);
+}
+function crevette_sidebar_facebook(){
+  $facebookAccount = esc_attr(get_option('facebook_account'));
+  echo '<input type="text" name"twitter_account" placeholder="Adresse Facebook" value="'.$facebookAccount.'">';
+  var_dump($facebookAccount);
+}
+function crevette_sidebar_twitter(){
+  $twitterAccount = esc_attr(get_option('twitter_account'));
+  echo '<input type="text" name"twitter_account" placeholder="Compte Twitter" value="'.$twitterAccount.'">';
+  var_dump($twitterAccount);
 }
 function crevette_sidebar_options(){
   echo 'Paramétrer les options générales du Thème CrevetteRose';
 }
 function crevette_sidebar_name(){
   $firstName = esc_attr(get_option('first_name'));
-  echo '<input type="text" name"first_name" placeholder="Met ton prénom" value="'.$firstName.'">';
+  $lastName = esc_attr(get_option('last_name'));
+  echo '<input type="text" name"first_name" placeholder="Prénom" value="'.$firstName.'"> <input type="text" name"last_name" placeholder="Nom" value="'.$lastName.'">';
+  var_dump($firstName, $lastName);
 }
 ?>
--- inc/templates/crevette-admin.php
+++ inc/templates/crevette-admin.php
@@ -7,13 +7,14 @@
 <body>
   <h1>Options du thème <?php bloginfo('name');?></h1>
   <p class="descritpion">Cette page permet de gérer les options paramétrables du thème !</p>
-  <?php settings_errors( $setting, $sanitize, $hide_on_update );?>
-  <?php $firstName = esc_attr(get_option('first_name'));?>
+  
+  <?php settings_errors();?>
 
-  <form action="options.php" method="POST">
+  <form method="post" action="options.php" >
     <?php settings_fields('crevette-settings-group');?>
     <?php do_settings_sections( 'crevette_theme_settings' ); ?>
     <?php submit_button('Enregistrer les modifications');?>
   </form>
+
 </body>
 </html>
